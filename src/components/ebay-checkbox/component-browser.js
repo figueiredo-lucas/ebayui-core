7f753b4eab0d3e5f4a4476f2da0df93c563c3e64
LuLaValva <lukelavalva@gmail.com>
2023-02-18 20:51:38 -0800

    docs(checkbox): add JSDoc

--- src/components/ebay-checkbox/component-browser.js
+++ src/components/ebay-checkbox/component-browser.js
@@ -1,10 +1,19 @@
+// @ts-check
+
 export default {
     handleChange: forwardEvent('change'),
     handleFocus: forwardEvent('focus'),
     handleKeydown: forwardEvent('keydown'),
 };
 
+/**
+ * @param {string} eventName
+ */
 function forwardEvent(eventName) {
+    /**
+     * @param {Event} originalEvent
+     * @param {HTMLInputElement} el
+     */
     return function (originalEvent, el) {
         const value = (el || this.el.querySelector('input')).value;
         const checked = (el || this.el.querySelector('input')).checked;
